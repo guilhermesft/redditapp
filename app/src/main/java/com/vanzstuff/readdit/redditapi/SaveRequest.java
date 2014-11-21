b88c706f89c593a4060e9676654e22a11f60c8dc
José Guilherme Vanz <jose.vanz@itslemonade.com>
2014-11-21 08:34:58 -0200

    SaveRequest
    Adding the save request. Used to save a link or comment. #5

---
+++ app/src/main/java/com/vanzstuff/readdit/redditapi/SaveRequest.java
@@ -0,0 +1,22 @@
+package com.vanzstuff.readdit.redditapi;
+
+import com.android.volley.Response;
+
+import org.json.JSONObject;
+
+import java.util.Map;
+
+/**
+ * Created by vanz on 21/11/14.
+ */
+public class SaveRequest extends BaseRedditApiJsonRequest {
+
+    public static final String PARAM_CATEGORY = "category";
+    public static final String PARAM_ID = "id";
+    public static final String PARAM_UH = "uh";
+
+    public SaveRequest(Response.Listener<JSONObject> listener, Response.ErrorListener errorListener, Map<String, Object> params) {
+        super(Method.POST, "api/save" , null, listener, errorListener, params);
+    }
+
+}
