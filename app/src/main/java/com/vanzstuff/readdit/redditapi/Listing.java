ecd86fa93874a6d9c2e4b833fa1343a918042286
José Guilherme Vanz <guilherme.sft@gmail.com>
2014-11-24 12:44:15 -0200

    Retrieve inbox/unread/sent messages
    Added request to retrieve inbox/unread/sent messages. #12

---
+++ app/src/main/java/com/vanzstuff/readdit/redditapi/Listing.java
@@ -0,0 +1,15 @@
+package com.vanzstuff.readdit.redditapi;
+
+/**
+ * Created by vanz on 24/11/14.
+ */
+public interface Listing {
+
+    public static final String PARAM_AFTER = "after";
+    public static final String PARAM_BEFORE = "before";
+    public static final String PARAM_COUNT = "count";
+    public static final String PARAM_LIMIT = "limit";
+    public static final String PARAM_SHOW = "show";
+
+}
+
---
+++ app/src/main/java/com/vanzstuff/readdit/redditapi/MessageRequest.java
@@ -0,0 +1,25 @@
+package com.vanzstuff.readdit.redditapi;
+
+import com.android.volley.Response;
+
+import org.json.JSONObject;
+
+import java.util.Map;
+
+/**
+ * Created by vanz on 24/11/14.
+ */
+public class MessageRequest extends BaseRedditApiJsonRequest implements Listing{
+
+    public static final String PARAM_MARK = "mark";
+    public static final String PARAM_MID = "mid";
+    public static final String URL_MESSAGE_INBOX = "/message/inbox";
+    public static final String URL_MESSAGE_UNREAD = "/message/unread";
+    public static final String URL_MESSAGE_SENT = "/message/sent";
+
+
+    public MessageRequest(String path, Response.Listener<JSONObject> listener, Response.ErrorListener errorListener, Map<String, Object> params) {
+        super(Method.GET, path , null, listener, errorListener, params);
+    }
+
+}
