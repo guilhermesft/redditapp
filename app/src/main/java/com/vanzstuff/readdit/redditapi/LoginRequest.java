7e233ac21a27e5044badd4cb7152b8525b70d46a
José Guilherme Vanz <guilherme.sft@gmail.com>
2014-11-24 08:33:04 -0200

    Login request params
    Adding constants with params names

--- app/src/main/java/com/vanzstuff/readdit/redditapi/LoginRequest.java
+++ app/src/main/java/com/vanzstuff/readdit/redditapi/LoginRequest.java
@@ -10,6 +10,10 @@ import java.util.Map;
  */
 public class LoginRequest extends BaseRedditApiJsonRequest {
 
+    public static final String PARAM_USER = "user";
+    public static final String PARAM_PASSWORD = "passwd";
+    public static final String PARAM_REM = "rem";
+
     public LoginRequest(Response.Listener<JSONObject> listener, Response.ErrorListener errorListener, Map<String, Object> params) {
         super(Method.POST, "api/login" , null, listener, errorListener, params);
     }
