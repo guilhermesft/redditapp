0ffb81702d89f03b4747ff4a49059a097b861a73
José Guilherme Vanz <guilherme.sft@gmail.com>
2014-11-24 08:27:14 -0200

    Comment and reply request
    Added request to comment and reply comments. #9

---
+++ app/src/main/java/com/vanzstuff/readdit/redditapi/CommentReplyRequest.java
@@ -0,0 +1,21 @@
+package com.vanzstuff.readdit.redditapi;
+
+import com.android.volley.Response;
+
+import org.json.JSONObject;
+
+import java.util.Map;
+
+/**
+ * Created by vanz on 24/11/14.
+ */
+public class CommentReplyRequest extends BaseRedditApiJsonRequest {
+
+    public static final String PARAM_TEXT = "text";
+    public static final String PARAM_THING_ID = "thing_id";
+    public static final String PARAM_UH = "uh";
+
+    public CommentReplyRequest(Response.Listener<JSONObject> listener, Response.ErrorListener errorListener, Map<String, Object> params) {
+        super(Method.POST, "/api/comment", null, listener, errorListener, params);
+    }
+}
