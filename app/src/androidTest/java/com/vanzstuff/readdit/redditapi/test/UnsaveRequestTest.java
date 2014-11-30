7a05ce023d1f7bde565478a5defb235b40c62b12
José Guilherme Vanz <guilherme.sft@gmail.com>
2014-11-30 17:52:19 -0200

    UnsaveRequest
    Added the tests of UnsaveRequest.
    
    Closes #6

---
+++ app/src/androidTest/java/com/vanzstuff/readdit/redditapi/test/UnsaveRequestTest.java
@@ -0,0 +1,76 @@
+package com.vanzstuff.readdit.redditapi.test;
+
+import android.net.Uri;
+import android.test.AndroidTestCase;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+import com.vanzstuff.readdit.redditapi.SaveRequest;
+import com.vanzstuff.readdit.redditapi.UnsaveRequest;
+import com.vanzstuff.readditapp.test.mocks.HttpStackMock;
+import com.vanzstuff.readditapp.test.mocks.Util;
+
+import org.apache.http.ProtocolVersion;
+import org.apache.http.message.BasicHttpResponse;
+import org.apache.http.message.BasicStatusLine;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by vanz on 30/11/14.
+ */
+public class UnsaveRequestTest extends AndroidTestCase {
+
+    private HttpStackMock mMockStack;
+    private RequestQueue mQueue;
+    private Map<String, Object> mFakeParams;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mMockStack = new HttpStackMock();
+        mQueue = Volley.newRequestQueue(getContext(), mMockStack);
+        mQueue.start();
+        mFakeParams = new HashMap<String, Object>();
+
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        mQueue.stop();
+    }
+
+    public void testRequest() throws AuthFailureError {
+        mFakeParams.put(UnsaveRequest.PARAM_ID, "thing");
+        mFakeParams.put(SaveRequest.PARAM_UH, "fakeToken");
+        mMockStack.setResponse(new BasicHttpResponse(new BasicStatusLine(new ProtocolVersion("HTTP", 1, 1), 200, "OK")));
+        UnsaveRequest request = new UnsaveRequest(new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                assertNotNull(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                fail(error.getMessage());
+            }
+        }, mFakeParams);
+        mQueue.add(request);
+        while ( mMockStack.getLastRequest() == null);
+        Uri requestUrl = Uri.parse(mMockStack.getLastRequest().getUrl());
+        assertEquals( "https", requestUrl.getScheme());
+        assertEquals("oauth.reddit.com", requestUrl.getAuthority());
+        assertEquals( "/api/unsave", requestUrl.getPath());
+        assertEquals("application/x-www-form-urlencoded; charset=UTF-8", mMockStack.getLastRequest().getBodyContentType());
+        Map<String, String> postParams = Util.getPostParams(new String(mMockStack.getLastRequest().getBody()));
+        assertEquals(2, postParams.size());
+        assertEquals(mFakeParams.get(UnsaveRequest.PARAM_ID), postParams.get(UnsaveRequest.PARAM_ID));
+        assertEquals(mFakeParams.get(UnsaveRequest.PARAM_UH), postParams.get(UnsaveRequest.PARAM_UH));
+    }
+}
