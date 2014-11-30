00311d5c9f4ba728826f4b00b576ba9d9f50d3bd
José Guilherme Vanz <guilherme.sft@gmail.com>
2014-11-30 17:48:09 -0200

    Vote request tests
    Added the tests of VoteRequest.
    
    Closes #4

---
+++ app/src/androidTest/java/com/vanzstuff/readdit/redditapi/test/VoteRequestTest.java
@@ -0,0 +1,77 @@
+package com.vanzstuff.readdit.redditapi.test;
+
+import android.net.Uri;
+import android.test.AndroidTestCase;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+import com.vanzstuff.readdit.redditapi.VoteRequest;
+import com.vanzstuff.readditapp.test.mocks.HttpStackMock;
+import com.vanzstuff.readditapp.test.mocks.Util;
+
+import org.apache.http.ProtocolVersion;
+import org.apache.http.message.BasicHttpResponse;
+import org.apache.http.message.BasicStatusLine;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by vanz on 30/11/14.
+ */
+public class VoteRequestTest extends AndroidTestCase {
+
+    private HttpStackMock mMockStack;
+    private RequestQueue mQueue;
+    private Map<String, Object> mFakeParams;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mMockStack = new HttpStackMock();
+        mQueue = Volley.newRequestQueue(getContext(), mMockStack);
+        mQueue.start();
+        mFakeParams = new HashMap<String, Object>();
+
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        mQueue.stop();
+    }
+
+    public void testRequestVote() throws AuthFailureError {
+        mFakeParams.put(VoteRequest.PARAM_DIR, VoteRequest.VOTE_UP);
+        mFakeParams.put(VoteRequest.PARAM_ID, "fakeThing");
+        mFakeParams.put(VoteRequest.PARAM_UH, "fakeToken");
+        mMockStack.setResponse(new BasicHttpResponse(new BasicStatusLine(new ProtocolVersion("HTTP", 1, 1), 200, "OK")));
+        VoteRequest request = new VoteRequest(new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                assertNotNull(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                fail(error.getMessage());
+            }
+        }, mFakeParams);
+        mQueue.add(request);
+        while (mMockStack.getLastRequest() == null);
+        Uri requestUrl = Uri.parse(mMockStack.getLastRequest().getUrl());
+        assertEquals( "https", requestUrl.getScheme());
+        assertEquals("oauth.reddit.com", requestUrl.getAuthority());
+        assertEquals( "/api/vote", requestUrl.getPath());
+        assertEquals("application/x-www-form-urlencoded; charset=UTF-8", mMockStack.getLastRequest().getBodyContentType());
+        Map<String, String> postParams = Util.getPostParams(new String(mMockStack.getLastRequest().getBody()));
+        assertEquals(3, postParams.size());
+        assertEquals(mFakeParams.get(VoteRequest.PARAM_DIR), Integer.parseInt(postParams.get(VoteRequest.PARAM_DIR)));
+        assertEquals(mFakeParams.get(VoteRequest.PARAM_ID), postParams.get(VoteRequest.PARAM_ID));
+        assertEquals(mFakeParams.get(VoteRequest.PARAM_UH), postParams.get(VoteRequest.PARAM_UH));
+    }
+}
--- app/src/main/java/com/vanzstuff/readdit/redditapi/VoteRequest.java
+++ app/src/main/java/com/vanzstuff/readdit/redditapi/VoteRequest.java
@@ -14,8 +14,12 @@ public class VoteRequest extends BaseRedditApiJsonRequest {
     public static final String PARAM_DIR = "dir";
     public static final String PARAM_ID = "id";
     public static final String PARAM_UH = "uh";
+    public static final int VOTE_UP = 1;
+    public static final int VOTE_NONE = 0;
+    public static final int VOTE_DOWN = -1;
 
     public VoteRequest(Response.Listener<JSONObject> listener, Response.ErrorListener errorListener, Map<String, Object> params) {
         super(Method.POST, "api/vote" , null, listener, errorListener, params);
     }
 }
+
