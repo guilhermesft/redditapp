d57d0728194c9a28dce23888b0e73423c2652fa5
José Guilherme Vanz <guilherme.sft@gmail.com>
2014-12-01 07:49:59 -0200

    Subscribe/unsubscribe tests
    Added the tests of SubscribeRequest.
    
    Closes #15

---
+++ app/src/androidTest/java/com/vanzstuff/readdit/redditapi/test/SubscribeRequestTest.java
@@ -0,0 +1,78 @@
+package com.vanzstuff.readdit.redditapi.test;
+
+import android.net.Uri;
+import android.test.AndroidTestCase;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+import com.vanzstuff.readdit.redditapi.SaveRequest;
+import com.vanzstuff.readdit.redditapi.SubscribeRequest;
+import com.vanzstuff.readditapp.test.mocks.HttpStackMock;
+import com.vanzstuff.readditapp.test.mocks.Util;
+
+import org.apache.http.ProtocolVersion;
+import org.apache.http.message.BasicHttpResponse;
+import org.apache.http.message.BasicStatusLine;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by vanz on 30/11/14.
+ */
+public class SubscribeRequestTest extends AndroidTestCase {
+
+    private HttpStackMock mMockStack;
+    private RequestQueue mQueue;
+    private Map<String, Object> mFakeParams;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mMockStack = new HttpStackMock();
+        mQueue = Volley.newRequestQueue(getContext(), mMockStack);
+        mQueue.start();
+        mFakeParams = new HashMap<String, Object>();
+
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        mQueue.stop();
+    }
+
+    public void testRequest() throws AuthFailureError {
+        mFakeParams.put(SubscribeRequest.PARAM_ACTION, SubscribeRequest.ACTION_SUBSCRIBE);
+        mFakeParams.put(SubscribeRequest.PARAM_SR, "subreddit");
+        mFakeParams.put(SubscribeRequest.PARAM_UH, "faketoken");
+        mMockStack.setResponse(new BasicHttpResponse(new BasicStatusLine(new ProtocolVersion("HTTP", 1, 1), 200, "OK")));
+        SubscribeRequest request = new SubscribeRequest(new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                assertNotNull(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                fail(error.getMessage());
+            }
+        }, mFakeParams);
+        mQueue.add(request);
+        while ( mMockStack.getLastRequest() == null);
+        Uri requestUrl = Uri.parse(mMockStack.getLastRequest().getUrl());
+        assertEquals( "https", requestUrl.getScheme());
+        assertEquals("oauth.reddit.com", requestUrl.getAuthority());
+        assertEquals( "/api/subscribe", requestUrl.getPath());
+        assertEquals("application/x-www-form-urlencoded; charset=UTF-8", mMockStack.getLastRequest().getBodyContentType());
+        Map<String, String> postParams = Util.getPostParams(new String(mMockStack.getLastRequest().getBody()));
+        assertEquals(3, postParams.size());
+        assertEquals(mFakeParams.get(SubscribeRequest.PARAM_ACTION), postParams.get(SubscribeRequest.PARAM_ACTION));
+        assertEquals(mFakeParams.get(SubscribeRequest.PARAM_SR), postParams.get(SubscribeRequest.PARAM_SR));
+        assertEquals(mFakeParams.get(SubscribeRequest.PARAM_UH), postParams.get(SubscribeRequest.PARAM_UH));
+    }
+}
--- app/src/main/java/com/vanzstuff/readdit/redditapi/SubscribeRequest.java
+++ app/src/main/java/com/vanzstuff/readdit/redditapi/SubscribeRequest.java
@@ -14,6 +14,8 @@ public class SubscribeRequest extends BaseRedditApiJsonRequest {
     public static final String PARAM_ACTION = "action";
     public static final String PARAM_SR = "sr";
     public static final String PARAM_UH = "uh";
+    public static final String ACTION_SUBSCRIBE = "sub";
+    public static final String ACTION_UNSUBSCRIBE = "usub";
 
     public SubscribeRequest(Response.Listener<JSONObject> listener, Response.ErrorListener errorListener, Map<String, Object> params) {
         super(Method.POST, "/api/subscribe", null, listener, errorListener, params);
