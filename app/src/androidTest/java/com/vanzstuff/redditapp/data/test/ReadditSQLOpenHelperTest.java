bb795b5b4f56a2e44f39b40c13761e4a9bcf4bc9
José Guilherme Vanz <guilherme.sft@gmail.com>
2014-12-02 08:32:02 -0200

    ReadditSQLOpenHelper implementation
    Fix some problems detected with the tests in the ReadditSQLOpenHelper and add the tests themselves. #3

--- app/src/androidTest/java/com/vanzstuff/redditapp/data/test/ReadditSQLOpenHelperTest.java
+++ app/src/androidTest/java/com/vanzstuff/redditapp/data/test/ReadditSQLOpenHelperTest.java
@@ -1,15 +1,55 @@
 package com.vanzstuff.redditapp.data.test;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
 
 import com.vanzstuff.readditapp.data.ReadditSQLOpenHelper;
+import com.vanzstuff.redditapp.data.ReadditContract;
 
 /**
  * Created by vanz on 16/11/14.
  */
 public class ReadditSQLOpenHelperTest extends AndroidTestCase{
 
+    private ReadditSQLOpenHelper mDBHelper;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        getContext().deleteDatabase("readdit.db");
+        mDBHelper= new ReadditSQLOpenHelper(getContext());
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        mDBHelper.close();
+        mDBHelper = null;
+    }
+
     public void testOnCreate(){
-        ReadditSQLOpenHelper dbHelper = new ReadditSQLOpenHelper(getContext());
+        SQLiteDatabase db = mDBHelper.getWritableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM main.sqlite_master WHERE type=?;", new String[]{"table"});
+        assertEquals(7, cursor.getCount());
+        while (cursor.moveToNext()){
+            if ( cursor.getString(1).equals(ReadditContract.Subscribe.TABLE_NAME)){
+                assertEquals("CREATE TABLE subscribe ( _id INTEGER PRIMARY KEY, subreddit TEXT NOT NULL )", cursor.getString(4));
+            }else if (cursor.getString(1).equals(ReadditContract.Comment.TABLE_NAME)){
+                assertEquals("CREATE TABLE comment ( _id INTEGER PRIMARY KEY,parent INTEGER REFERENCES comment ( _id ),content TEXT NOT NULL,date TEXT NOT NULL, user TEXT NOT NULL, post INTEGER REFERENCES post ( _id ))", cursor.getString(4));
+            }else if( cursor.getString(1).equals(ReadditContract.Post.TABLE_NAME)) {
+                assertEquals("CREATE TABLE post ( _id INTERGER PRIMARY KEY, content TEXT NOT NULL, date TEXT NOT NULL, subredditINTEGER REFERENCES subreddit ( _id ),user TEXT NOT NULL,votes INTEGER DEFAULT 0)", cursor.getString(4));
+            }else if(cursor.getString(1).equals(ReadditContract.Subreddit.TABLE_NAME)) {
+                assertEquals("CREATE TABLE subreddit ( _id INTEGER PRIMARY KEY, name TEXT NOT NULL UNIQUE)", cursor.getString(4));
+            }else if(cursor.getString(1).equals(ReadditContract.Tag.TABLE_NAME)) {
+                assertEquals("CREATE TABLE tag ( _id INTEGER PRIMARY KEY, name TEXT NOT NULL )", cursor.getString(4));
+            }else if(cursor.getString(1).equals(ReadditContract.TagXPost.TABLE_NAME)) {
+                assertEquals("CREATE TABLE tag_x_post ( tag INTEGER REFERENCES tag( _id) , post INTEGER REFERENCES post( _id), PRIMARY KEY ( tag, post ))", cursor.getString(4));
+            }else if (cursor.getString(1).equals("android_metadata")) {
+                continue;
+            }else{
+                fail("Unexpected table: " + cursor.getString(0));
+            }
+        }
     }
 }
--- app/src/main/java/com/vanzstuff/readditapp/data/ReadditSQLOpenHelper.java
+++ app/src/main/java/com/vanzstuff/readditapp/data/ReadditSQLOpenHelper.java
@@ -28,14 +28,15 @@ public class ReadditSQLOpenHelper extends SQLiteOpenHelper {
                 ReadditContract.Subreddit.COLUMN_NAME + " TEXT NOT NULL UNIQUE);";
         final String CREATE_POST = "CREATE TABLE " + ReadditContract.Post.TABLE_NAME + " ( " +
                 ReadditContract.Post._ID + " INTERGER PRIMARY KEY, " +
-                ReadditContract.Post.COLUMN_CONTENT + " TEXT NOT NULL" +
-                ReadditContract.Post.COLUMN_DATE + " TEXT NOT NULL" + //TODO text type is the best choice?
+                ReadditContract.Post.COLUMN_CONTENT + " TEXT NOT NULL, " +
+                ReadditContract.Post.COLUMN_DATE + " TEXT NOT NULL, " + //TODO text type is the best choice?
                 ReadditContract.Post.COLUMN_SUBREDDIT + "INTEGER REFERENCES " + ReadditContract.Subreddit.TABLE_NAME + " ( " + ReadditContract.Subreddit._ID + " )," +
                 ReadditContract.Post.COLUMN_USER +  " TEXT NOT NULL," +
                 ReadditContract.Post.COLUMN_VOTES + " INTEGER DEFAULT 0);";
-        final String CREATE_TAG_X_POST = "CREATE TABLE " + ReadditContract.TagVsPost.TABLE_NAME + " ( " +
-                ReadditContract.TagVsPost.COLUMN_TAG + " INTEGER REFERENCES " + ReadditContract.Tag.TABLE_NAME + "( " + ReadditContract.Tag._ID + ") , " +
-                ReadditContract.TagVsPost.COLUMN_POST + " INTEGER REFERENCES " + ReadditContract.Post.TABLE_NAME + "( " + ReadditContract.Post._ID + "));";
+        final String CREATE_TAG_X_POST = "CREATE TABLE " + ReadditContract.TagXPost.TABLE_NAME + " ( " +
+                ReadditContract.TagXPost.COLUMN_TAG + " INTEGER REFERENCES " + ReadditContract.Tag.TABLE_NAME + "( " + ReadditContract.Tag._ID + ") , " +
+                ReadditContract.TagXPost.COLUMN_POST + " INTEGER REFERENCES " + ReadditContract.Post.TABLE_NAME + "( " + ReadditContract.Post._ID + "), " +
+                "PRIMARY KEY ( " + ReadditContract.TagXPost.COLUMN_TAG + ", " + ReadditContract.TagXPost.COLUMN_POST + " ));";
         final String CREATE_COMMENT = "CREATE TABLE " + ReadditContract.Comment.TABLE_NAME + " ( " +
                 ReadditContract.Comment._ID + " INTEGER PRIMARY KEY," +
                 ReadditContract.Comment.COLUMN_PARENT + " INTEGER REFERENCES " + ReadditContract.Comment.TABLE_NAME + " ( " + ReadditContract.Comment._ID + " )," +
@@ -43,12 +44,16 @@ public class ReadditSQLOpenHelper extends SQLiteOpenHelper {
                 ReadditContract.Comment.COLUMN_DATE + " TEXT NOT NULL, " +
                 ReadditContract.Comment.COLUMN_USER + " TEXT NOT NULL, " +
                 ReadditContract.Comment.COLUMN_POST + " INTEGER REFERENCES " + ReadditContract.Post.TABLE_NAME + " ( " + ReadditContract.Post._ID + " ));";
+        final String CREATE_SUBSCRIBE = "CREATE TABLE " + ReadditContract.Subscribe.TABLE_NAME + " ( " +
+                ReadditContract.Subscribe._ID + " INTEGER PRIMARY KEY, " +
+                ReadditContract.Subscribe.COLUMN_SUBREDDIT + " TEXT NOT NULL );";
 
         db.execSQL(CREATE_TAG);
         db.execSQL(CREATE_SUBREDDIT);
         db.execSQL(CREATE_POST);
         db.execSQL(CREATE_TAG_X_POST);
         db.execSQL(CREATE_COMMENT);
+        db.execSQL(CREATE_SUBSCRIBE);
     }
 
     @Override
