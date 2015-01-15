fbd06debfa9c5fd77c3994b12df2aba9f9d45d36
José Guilherme Vanz <guilherme.sft@gmail.com>
2015-01-15 20:23:32 -0200

    Vote button
    Implemented the method to vote in the post shown in the DetailFragment

--- app/src/main/java/com/vanzstuff/readdit/fragments/DetailFragment.java
+++ app/src/main/java/com/vanzstuff/readdit/fragments/DetailFragment.java
@@ -2,6 +2,7 @@ package com.vanzstuff.readdit.fragments;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewOutlineProvider;
 import android.webkit.WebView;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
@@ -17,7 +19,9 @@ import android.widget.TextView;
 
 import com.vanzstuff.readdit.Logger;
 import com.vanzstuff.readdit.PredefinedTags;
+import com.vanzstuff.readdit.UserSession;
 import com.vanzstuff.readdit.data.ReadditContract;
+import com.vanzstuff.readdit.redditapi.VoteRequest;
 import com.vanzstuff.redditapp.R;
 
 /**
@@ -117,10 +121,10 @@ public class DetailFragment extends Fragment implements View.OnClickListener {
     public void onClick(View v) {
         if ( v.getId() == R.id.action_menu_up_vote){
             Logger.d("up vote");
-            //TODO
+            vote(VoteRequest.VOTE_UP);
         } else if ( v.getId() == R.id.action_menu_down_vote){
             Logger.d("down vote");
-            //TODO
+            vote(VoteRequest.VOTE_DOWN);
         }else if ( v.getId() == R.id.action_menu_save){
             //add the tag saved to the post
             getActivity().getContentResolver().insert(ReadditContract.Post.buildAddTagUri(mPostID, PredefinedTags.SAVED.getName()), null);
@@ -130,17 +134,31 @@ public class DetailFragment extends Fragment implements View.OnClickListener {
         }else if ( v.getId() == R.id.action_menu_label){
             Logger.d("label vote");
             InputTagFragment.newInstance(mPostID).show(getActivity().getSupportFragmentManager(), "InputTagFragment");
-        }else{
-            Logger.d("What?");
         }
     }
 
     /**
-     * This method open a dialog for the user enter the labels and persistem in the database
+     * Method insert or update the user vote in the mPostID
+     * @param voteDirection vote direction from VoteRequest.VOTE_UP or VoteRequest.VOTE_DOWN
      */
-    private void addLabels() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-
+    private void vote(int voteDirection){
+        Cursor cursor = getActivity().getContentResolver().query(ReadditContract.Vote.CONTENT_URI, new String[]{ReadditContract.Vote._ID},
+                ReadditContract.Vote.COLUMN_POST + "=? AND " + ReadditContract.Vote.COLUMN_USER + "=?",
+                new String[]{String.valueOf(mPostID), String.valueOf(UserSession.getInstance().getCurrentUserInfo().id)},
+                null);
+        ContentValues values = new ContentValues(2);
+        values.put(ReadditContract.Vote.COLUMN_USER, UserSession.getInstance().getCurrentUserInfo().id);
+        values.put(ReadditContract.Vote.COLUMN_POST, mPostID);
+        values.put(ReadditContract.Vote.COLUMN_DIRECTION, voteDirection);
+        if ( cursor.moveToFirst()){
+            //user already has voted in the post. Update de vote
+            getActivity().getContentResolver().update(ReadditContract.Vote.CONTENT_URI, values,ReadditContract.Vote.COLUMN_POST + "=? AND " + ReadditContract.Vote.COLUMN_USER + "=?",
+                    new String[]{String.valueOf(mPostID), String.valueOf(UserSession.getInstance().getCurrentUserInfo().id)});
+        } else {
+            //user has not voted in the post. Insert a vote
+            getActivity().getContentResolver().insert(ReadditContract.Vote.CONTENT_URI, values);
+        }
+        cursor.close();
     }
 
     /**
